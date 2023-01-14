e08242f5e32954608f679e668e782b0cfa9f3874
XSavier7 <kuznecovartem752@gmail.com>
2023-01-14 20:42:46 +0200

    patau

---
+++ src/Jopa.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import '../src/Jopa.css'
+
+function Jopa() {
+    return (
+        <div className="centersosis"> 
+            <p className="sosiska">sosis</p>
+            <h2 className="popka">xuy</h2>
+        </div>
+    )
+}
+
+export default Jopa
